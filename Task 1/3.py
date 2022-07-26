51024010bedebc9c60715dd1188889181dc59594
Piyush <75735209+PiyushBL45t@users.noreply.github.com>
2022-07-26 13:56:08 +0530

    Add files via upload

---
+++ Task 1/3.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    summation = a + b
+    subtraction = a - b
+    product = a * b
+    print(summation)
+    print(subtraction)
+    print(product)
---
+++ Task 1/4.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    int_div = a/b
+    float_div = a//b
+    print(float_div)
+    print(int_div)
